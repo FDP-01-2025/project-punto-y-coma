f82a427484aad13c4c8938dbac063ab636bd4816
lucianavarro25 <00109525@uca.edu.sv>
2025-06-16 08:12:41 -0600

    Introduccion

--- src/intro.h
+++ src/intro.h
@@ -7,5 +7,5 @@ string nombreJugador; // Variable global para guardar el nombre del jugador
 void introduccionJuego() {
     cout << "Antes de comenzar, dime tu nombre: ";
     cin >> nombreJugador;
-    cout << "\nMuy bien, " << nombreJugador << ". Prepárate para tu destino...\n";
+    cout << "\nMuy bien, " << nombreJugador << ". Preparate para tu destino...\n";
 }
